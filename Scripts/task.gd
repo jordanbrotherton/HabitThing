35f14fbe16e2ab2d63ec48b1ee4cb575b23e1e6b
Jordan <jordanjbrotherton@proton.me>
2025-09-27 13:03:18 -0400

    fix: Update task.gd to have enum

--- Scripts/task.gd
+++ Scripts/task.gd
@@ -1,5 +1,8 @@
 class_name Task extends Node
 
+enum repeatTypes { NONE, DAILY, WEEKLY }
+
 var taskName = "Task"
 var dueDate = Time.get_unix_time_from_datetime_string("1970-01-01T10:00:00")
-var repeating = false
+var repeatType = repeatTypes.NONE
+
