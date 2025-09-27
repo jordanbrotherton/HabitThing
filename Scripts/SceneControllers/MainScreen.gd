46bb9816b7550f29e16f3e11b55e0516411a325b
Jordan <jordanjbrotherton@proton.me>
2025-09-27 14:40:19 -0400

    feat: Added many UI shells

---
+++ Scripts/SceneControllers/MainScreen.gd
@@ -0,0 +1,9 @@
+extends Control
+
+
+func _on_add_clicked() -> void:
+	get_tree().change_scene_to_file("res://Scenes/TaskMaker.tscn")
+
+
+func _on_shop_clicked() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Shop.tscn")
