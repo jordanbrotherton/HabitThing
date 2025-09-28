9d5b14bc7ce1f9ade56107f67acb2c5ab9cecc5c
bedelln <nathanb042806@gmail.com>
2025-09-28 03:25:29 -0400

    Edited

--- Scripts/SceneControllers/MainScreen.gd
+++ Scripts/SceneControllers/MainScreen.gd
@@ -15,10 +15,10 @@ func _on_add_clicked() -> void:
 func _on_shop_clicked() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Shop.tscn")
 
-
 func _on_stats_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Stats.tscn")
 
 
 func _on_texture_rect_ready() -> void:
 	$VBoxContainer/ScrollContainer/VBoxContainer/TextureRect.texture = load(ControlsData.currHat)
+	queue_redraw()
